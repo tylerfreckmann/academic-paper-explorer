6b0c5002c13c8e08766e2f201b972767cf589f91
Tyler Freckmann <tyler.freckmann@gmail.com>
2024-09-09 10:04:18 -0700

    update for in-memory conversion with years 2020 onward

--- paper-scraper.py
+++ paper-scraper.py
@@ -1,35 +1,36 @@
 from bs4 import BeautifulSoup
-import os
+import pandas as pd
+import pymupdf
 import requests
-import shutil
 
-YEARS = [str(year) for year in range(2023, 2024)]
+YEARS = [str(year) for year in range(2020, 2024)]
 BASE_URL = 'https://papers.nips.cc/paper_files/paper/'
-PAPER_PDF_DIRECTORY = 'paper_pdfs'
 
-if os.path.exists(PAPER_PDF_DIRECTORY):
-    shutil.rmtree(PAPER_PDF_DIRECTORY)
-
-os.makedirs(PAPER_PDF_DIRECTORY)
-
+papers = []
 for year in YEARS:
-    os.makedirs(PAPER_PDF_DIRECTORY + '/' + year)
     response = requests.get(BASE_URL + year)
     soup = BeautifulSoup(response.content, 'lxml')
     # Paper link format: BASE_URL/{year}/hash/{hash}-Abstract-Conference.html
-    paper_list = [li.a for li in soup.select('.paper-list > li')]
+    paper_list = soup.find_all('a', title='paper title')
     for a in paper_list[:10]:
-        paper_name = a.string
+        paper = {}
+        paper['year'] = year
+        paper['name'] = a.string
         paper_link_components = a.get('href').split('/')[-1].split('-')
         paper_hash = paper_link_components[0]
-        paper_suffix = paper_link_components[2].split('.')[0]
-        file_path = PAPER_PDF_DIRECTORY + '/' + year + '/' + paper_name + '.pdf'
-        pdf_url = BASE_URL + year + '/file/' + paper_hash + '-Paper-' + paper_suffix + '.pdf'
+        if int(year) >= 2022:
+            paper_suffix = paper_link_components[2].split('.')[0]
+            pdf_url = BASE_URL + year + '/file/' + paper_hash + '-Paper-' + paper_suffix + '.pdf'
+        else:
+            pdf_url = BASE_URL + year + '/file/' + paper_hash + '-Paper.pdf'
         try:
             response = requests.get(pdf_url)
             response.raise_for_status()
-            with open(file_path, 'wb') as file:
-                file.write(response.content)
-            print(f"PDF downloaded and saved to '{file_path}'.")
+            pdf = pymupdf.Document(stream=response.content)
+            paper['text'] = chr(12).join([page.get_text() for page in pdf])
         except requests.exceptions.RequestException as e:
             print(f"Failed to download the PDF. Reason: {e}")
+        papers.append(paper)
+
+df = pd.DataFrame(papers)
+df.to_csv('papers.csv', index=False, header=True)
