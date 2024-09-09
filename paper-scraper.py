67b8f785999098e11124d709361c0cbcd00c2003
Tyler Freckmann <tyler.freckmann@gmail.com>
2024-09-09 10:34:10 -0700

    pull from year <=2020 using Metadata.json

--- paper-scraper.py
+++ paper-scraper.py
@@ -3,12 +3,11 @@ import pandas as pd
 import pymupdf
 import requests
 
-YEARS = [str(year) for year in range(2020, 2024)]
 BASE_URL = 'https://papers.nips.cc/paper_files/paper/'
 
 papers = []
-for year in YEARS:
-    response = requests.get(BASE_URL + year)
+for year in range(1987, 2024):
+    response = requests.get(BASE_URL + str(year))
     soup = BeautifulSoup(response.content, 'lxml')
     # Paper link format: BASE_URL/{year}/hash/{hash}-Abstract-Conference.html
     paper_list = soup.find_all('a', title='paper title')
@@ -18,18 +17,29 @@ for year in YEARS:
         paper['name'] = a.string
         paper_link_components = a.get('href').split('/')[-1].split('-')
         paper_hash = paper_link_components[0]
-        if int(year) >= 2022:
-            paper_suffix = paper_link_components[2].split('.')[0]
-            pdf_url = BASE_URL + year + '/file/' + paper_hash + '-Paper-' + paper_suffix + '.pdf'
+        paper_link_base_url = BASE_URL + str(year) + '/file/' + paper_hash
+        if year >= 2020:
+            if year >= 2022:
+                paper_suffix = paper_link_components[2].split('.')[0]
+                pdf_url = paper_link_base_url + '-Paper-' + paper_suffix + '.pdf'
+            else:
+                pdf_url = paper_link_base_url + '-Paper.pdf'
+            try:
+                response = requests.get(pdf_url)
+                response.raise_for_status()
+                pdf = pymupdf.Document(stream=response.content)
+                paper['text'] = chr(12).join([page.get_text() for page in pdf])
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to download the PDF. Reason: {e}")
         else:
-            pdf_url = BASE_URL + year + '/file/' + paper_hash + '-Paper.pdf'
-        try:
-            response = requests.get(pdf_url)
-            response.raise_for_status()
-            pdf = pymupdf.Document(stream=response.content)
-            paper['text'] = chr(12).join([page.get_text() for page in pdf])
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to download the PDF. Reason: {e}")
+            metadata_url = paper_link_base_url + '-Metadata.json'
+            try:
+                response = requests.get(metadata_url)
+                response.raise_for_status()
+                metadata_json = response.json()
+                paper['text'] = metadata_json['full_text']
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to download Metadata JSON. Reason: {e}")            
         papers.append(paper)
 
 df = pd.DataFrame(papers)
