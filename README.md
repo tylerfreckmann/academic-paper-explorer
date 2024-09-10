0119ce40d2312fc49f69b976bdbee493e9ca3d70
Tyler Freckmann <tyler.freckmann@gmail.com>
2024-09-10 11:44:54 -0700

    update README and add comments to paper-scraper.py

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# academic-paper-explorer
-This is project that explores using Generative AI and other NLP techniques to explore a collection of academic papers.
+# Academic Paper Explorer
+
+This project will explore how Generative AI and NLP can be used to make navigating a collection of academic papers easier for end users.
+
+## Collecting the Data
+
+The data used in this project comes from the [NeurIPS Conference Proceedings](https://papers.nips.cc/).
+
+All papers from 1987-2023 have been downloaded and stored in a CSV file on AWS S3. The CSV file is ~1 GB in size and contains ~20,000 rows. Each row contains three columns:
+
+ - `year` - the year the paper was published
+ - `name` - the title of the paper
+ - `text` - the full text of the paper
+
+[paper-scraper.py](https://github.com/tylerfreckmann/academic-paper-explorer/blob/main/paper-scraper.py) scrapes this data from the [NeurIPS Conference Proceedings website](https://papers.nips.cc/).
+
+For years <= 2019, the conference website provides a _metadata_ link for each paper that contains the paper's full text in a JSON object.
+
+For years >= 2020, only the paper PDFs are provided, so `paper-scraper.py` downloads the PDFs and uses [PyMuPDF](https://pymupdf.readthedocs.io/en/latest/index.html) to convert the PDFs into plain text.
+
+Once all papers have been downloaded and processed, they are stored in a Pandas DataFrame and saved to a CSV.
+
+### The Data File
+
+> [!NOTE]
+> The data CSV file is ~1 GB in size and may take some time to download.
+> The compressed data CSV file is ~300 MB.
+
+The data is stored in an AWS S3 bucket. You can download a CSV version of the data or a compressed CSV at the following links:
+
+ - [papers.csv](https://academic-paper-explorer.s3.us-east-2.amazonaws.com/papers.csv) ~1 GB
+ - [papers.csv.gzip](https://academic-paper-explorer.s3.us-east-2.amazonaws.com/papers.csv.gz) ~300MB
--- paper-scraper.py
+++ paper-scraper.py
@@ -7,21 +7,29 @@ from tqdm import tqdm
 BASE_URL = 'https://papers.nips.cc/paper_files/paper/'
 
 papers = []
+
 for year in range(1987, 2024):
     print(f"---- PROCESSING YEAR {year} ----")
+
+    # Get the list of papers for the given year
     response = requests.get(BASE_URL + str(year))
+
+    # Convert to HTML soup and get list of paper links
     soup = BeautifulSoup(response.content, 'lxml')
-    # Paper link format: BASE_URL/{year}/hash/{hash}-Abstract-Conference.html
-    paper_list = soup.find_all('a', title='paper title')
-    for a in tqdm(paper_list):
+    list_of_paper_links = soup.find_all('a', title='paper title')
+
+    for a in tqdm(list_of_paper_links):
         paper = {}
         paper['year'] = year
         paper['name'] = a.string
+
+        # Paper link format: BASE_URL/{year}/hash/{hash}-{suffix}.html
         paper_link_components = a.get('href').split('/')[-1].split('-')
         paper_hash = paper_link_components[0]
         paper_link_base_url = BASE_URL + str(year) + '/file/' + paper_hash
-        if year >= 2020:
-            if year >= 2022:
+
+        if year >= 2020: # Raw paper text not available - process paper pdfs
+            if year >= 2022: # URLS contain additional suffixes
                 paper_suffix = paper_link_components[2].split('.')[0]
                 pdf_url = paper_link_base_url + '-Paper-' + paper_suffix + '.pdf'
             else:
@@ -29,19 +37,23 @@ for year in range(1987, 2024):
             try:
                 response = requests.get(pdf_url)
                 response.raise_for_status()
+                # Stream response content into PDF doc
                 pdf = pymupdf.Document(stream=response.content)
+                # Convert PDF doc to raw text
                 paper['text'] = chr(12).join([page.get_text() for page in pdf])
             except requests.exceptions.RequestException as e:
                 print(f"Failed to download the PDF. Reason: {e}")
-        else:
+        else: # Raw paper text available via Metadata.json link
             metadata_url = paper_link_base_url + '-Metadata.json'
             try:
                 response = requests.get(metadata_url)
                 response.raise_for_status()
+                # Extract raw paper text from paper metadata.json
                 metadata_json = response.json()
                 paper['text'] = metadata_json['full_text']
             except requests.exceptions.RequestException as e:
-                print(f"Failed to download Metadata JSON. Reason: {e}")            
+                print(f"Failed to download Metadata JSON. Reason: {e}")
+                
         papers.append(paper)
 
 df = pd.DataFrame(papers)
